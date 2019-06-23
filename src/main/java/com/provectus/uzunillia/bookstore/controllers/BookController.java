94aa7273294071891338f76c90e9f8aef579b07e
Illia Uzun <uzun.illia.main@gmail.com>
2019-06-23 22:15:49 +0300

    filters and search

--- src/main/java/com/provectus/uzunillia/bookstore/controllers/BookController.java
+++ src/main/java/com/provectus/uzunillia/bookstore/controllers/BookController.java
@@ -8,8 +8,13 @@ import com.provectus.uzunillia.bookstore.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 @Controller
 public class BookController {
@@ -23,10 +28,36 @@ public class BookController {
     }
 
     @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("books", bookService.findAll());
+    public String index(@RequestParam(value = "title", defaultValue = "", required = false) String title,
+                        @RequestParam(value = "genre", defaultValue = "", required = false) String genre,
+                        @RequestParam(value = "author", defaultValue = "", required = false) String author,
+                        Model model) {
+        List<Book> books = filter(title, genre, author);
+        model.addAttribute("books", books);
         return "index";
     }
+
+    private List<Book> filter(String title, String genre, String author) {
+        ArrayList<Book> filteredList = new ArrayList<>();
+
+        if (bookService.findAll() != null) {
+            for (Book book : bookService.findAll()) {
+                boolean isTitleMatches = Objects.requireNonNull(book.getTitle()).toLowerCase().startsWith(title);
+                boolean isGenreMatches = Objects.requireNonNull(Arrays.toString(book.getGenre())).contains(genre);
+                boolean isAuthorMatches = Objects.requireNonNull(Arrays.toString(book.getAuthor())).contains(author);
+
+                if (isTitleMatches) {
+                    if (isAuthorMatches) {
+                        if (isGenreMatches) {
+                            filteredList.add(book);
+                        }
+                    }
+                }
+            }
+        }
+        return filteredList;
+    }
+
 
     @GetMapping("/book")
     public String viewBook(@RequestParam("id") Long id, Model model) {
@@ -47,8 +78,9 @@ public class BookController {
         return "add_book";
     }
 
-    @PostMapping("/add_book")
-    public String submitAdd(@ModelAttribute Book book) {
+    @PostMapping(value = "/add_book")
+    public String submitAdd(@RequestParam MultiValueMap<Integer, String> formData, @ModelAttribute Book book) {
+        System.out.println(formData.toString());
         bookService.save(book);
         return "redirect:/";
     }
